cf7ee5c3c222f6d06228112925e0f54e4a74928d
Oren Golan <111655672+OrenGolanProjects@users.noreply.github.com>
2025-09-03 19:02:23 +0300

    Delete dh-reporting-dh-reporting-mobile directory

--- dh-reporting-dh-reporting-mobile/src/database/index.js
+++
@@ -1,9 +0,0 @@
-// Export all database functions so other files can import them easily
-// Example: import { createUser, runMigrations } from './src/database';
-
-export * from './db.js';
-export * from './migrationRunner.js';
-export * from './repositories/userRepository.js';
-export * from './repositories/projectRepository.js';
-export * from './repositories/workHoursRepository.js';
-export * from './repositories/sessionRepository.js';
